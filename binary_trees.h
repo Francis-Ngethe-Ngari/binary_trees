a960ad07d0c8af107fbdec2403e3d19027594e62
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-27 02:45:47 +0300

    Added Function prototypes, created a struct data structure

---
+++ binary_trees.h
@@ -0,0 +1,32 @@
+#ifndef _BINARY_TREES_H
+#define _BINARY_TREES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * struct binary_tree_s - binary tree node.
+ *
+ * @n: Integer stored in the node.
+ * @parent: Pointer to parent node.
+ * @left: pointer to left child node.
+ * @right: pointer to right child node.
+ *
+ */ 
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/*Function Prototypes*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+
+#endif /*_BINARY_TREES_H*/
