30d9c2ece6e1e3702775ac921edcc3f45ee06af6
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-03-28 03:02:13 +0300

    Added description of a primary requirent the function must meet

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,22 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Function  inserts a
+ * binary_tree_insert_right - Function  inserts a
  * node as the right-child of another node.
  *
  * @parent: pointer to parent node.
  *
  * @value: integer to be inserted in the binary tree.
  *
+ * description: If parent already has a right-child,
+ * the new node must take its place, and the old
+ * right-child must be set as the right-child of the new node.
+ *
  * Return: pointer to the created node, or NULL on failure
- * or if parent is NULL
+ * or if parent is NULL.
+ *
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newNode;
+
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *newNode = binary_tree_node(parent, value);
+	newNode = binary_tree_node(parent, value);
 
 	if (newNode == NULL)
 		return (NULL);
